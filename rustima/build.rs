93c95418234b158244fa8dc361bec3b38297cea1
Lee-Zzun <ulysses8755@gmail.com>
2026-04-20 06:15:28 +0900

    Window에서 uv sync 시 에러 해결

--- rustima/build.rs
+++ rustima/build.rs
@@ -10,5 +10,6 @@ fn main() {
         .file("lbfgsb_c/miniCBLAS.c")
         .file("lbfgsb_c/timer.c")
         .flag_if_supported("-std=c11")
+        .flag_if_supported("/std:c11")
         .compile("lbfgsb");
 }
